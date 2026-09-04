13f3f911b052b7cc1e01b679c898c984deb5b2db
Phuong Do <dohuyphuong@gmail.com>
2026-09-04 10:29:30 +0000

    Upgrade 57

--- artifacts/quick-mark/babel.config.js
+++ artifacts/quick-mark/babel.config.js
@@ -5,5 +5,13 @@ module.exports = function (api) {
     // disabled in app.json because the current compiler beta cannot transform
     // some React Native/third-party components ("private prop" error).
     presets: ['babel-preset-expo'],
+    // Some installed Expo Go runtimes cannot parse private class fields that
+    // React Native 0.81 ships in its web API polyfills. Compile them away so
+    // the development bundle remains compatible with those runtimes.
+    plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ],
   };
 };
